3f99c9bb95215cad03f216bb2182e99f3bdde4f6
Adewale Ayokanmi Adeleye <Adewagold@yahoo.com>
2021-09-11 18:35:20 +0100

    Block exercise

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,16 +28,60 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+function createBlock(data){
+	let block = {
+		prevHash:Blockchain.blocks[Blockchain.blocks.length-1].hash,
+		index:Blockchain.blocks.length,
+		data:data,
+		timestamp:Date.now()
+	}
+	block.hash = blockHash(block)
+	Blockchain.blocks.push(block)
+	console.log(block)
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+for (let line of poem) {
+	createBlock(line)
+}
 
+console.log(createBlock("hello"));
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
+/**
+ * 
+ * data must be non-empty
+for the genesis block only, the hash must be "000000"
+prevHash must be non-empty
+index must be an integer >= 0
+the hash must match what recomputing the hash with blockHash(..) produces} block 
+ *
+ */
 // **********************************
+function verifyChain(blockchain){
+	for(let block of blockchain.blocks){
+		
+		if(block.index<0){return false}
+		if(block.index===0){
+			console.log(`Verifying genesis block ${JSON.stringify(block)}`);
+			if(block.hash!=="000000"){
+				return false;
+			}
+		}
+		if(block.index>0){
+			if(Blockchain.blocks[block.index-1].hash !== block.prevHash){return false}
+			console.log(`Verifying block ${JSON.stringify(block)}`);
+			if(block.data===""){return false}
+			if(block.prevHash===""){return false}
+		}
+		console.log(`Verified: block ${block.index}`);
+	}
+	return true
+}
+
 
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
+		JSON.stringify(bl)
 		// TODO: use block data to calculate hash
 	).digest("hex");
 }
